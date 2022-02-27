5a3f2bd03807f784b5bf58a6b15c3fa8dd4774d2
pang yew ken <ukenster@gmail.com>
2022-02-27 21:06:14 +0800

    Completed 56 review google interview

---
+++ 03-Big-O/56-review-google-interview.js
@@ -0,0 +1,29 @@
+// Naive
+function hasPairWithSum(arr, sum) {
+  let len = arr.length
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] === sum) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+// better
+function hasPairWithSum2(arr, sum) {
+  const mySet = new Set()
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    if (mySet.has(arr[i])) {
+      return true
+    }
+    mySet.add(sum - arr[i])
+    // console.log(`Element=${arr[i]}`)
+    // console.log(mySet)
+  }
+  return false
+}
+
+console.log(hasPairWithSum2([6, 4, 3, 2, 1, 7], 9))
