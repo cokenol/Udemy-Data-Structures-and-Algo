a8bda3cb563ae4bf833d0954557b370f55139a6c
pang yew ken <ukenster@gmail.com>
2022-03-01 12:49:42 +0800

    Completed 67 classes in javascript

---
+++ 06-arrrays/67-class-in-js-.js
@@ -0,0 +1,32 @@
+// reference type
+var object1 = { value: 10 }
+var object2 = object1
+var object3 = { value: 10 }
+
+// context
+
+// Scope
+
+// instantiation
+class Player {
+  constructor(name, type) {
+    this.name = name
+    this.type = type
+  }
+
+  introduce() {
+    console.log(`Hi I am ${this.name}, I'm a ${this.type}`)
+  }
+}
+
+class Wizard extends Player {
+  constructor(name, type) {
+    super(name, type)
+  }
+  play() {
+    console.log(`WEEEE I'm a ${this.type}`)
+  }
+}
+
+const wizard1 = new Wizard("Shelly", "Healer")
+const wizard2 = new Wizard("Shawn", "Dark Magic")
