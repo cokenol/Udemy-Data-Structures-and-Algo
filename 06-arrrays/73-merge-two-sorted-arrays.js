856c298780d4449ff75fb0c5ae37ceb1d7d64454
pang yew ken <ukenster@gmail.com>
2022-03-03 14:25:44 +0800

    Completed 73 Merge two sorted arrays

--- 06-arrrays/73-merge-two-sorted-arrays.js
+++ 06-arrrays/73-merge-two-sorted-arrays.js
@@ -1,3 +1,16 @@
-function mergeSortedArrays(arr1, arr2) {}
+function mergeSortedArrays(arr1, arr2) {
+  if (arr1.length == 0) {
+    return arr2
+  }
+  if (arr2.length == 0) {
+    return arr1
+  }
 
-mergeSortedArrays([0, 3, 4, 31], [4, 6, 30])
+  arr2.forEach(n => {
+    arr1.push(n)
+    console.log(`arr1=${arr1} n=${n}`)
+  })
+  return arr1.sort((a, b) => a - b)
+}
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]))
