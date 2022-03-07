c7967e6de558c8812b5363533e49b6c61349f9ad
pang yew ken <ukenster@gmail.com>
2022-03-07 21:58:10 +0800

    added instructor solution for exercise 85

--- 07-hash-tables/85-exercise-first-recurring-character.js
+++ 07-hash-tables/85-exercise-first-recurring-character.js
@@ -8,6 +8,7 @@
 // Given an array = [2,3,4,5]
 // Return undefined
 
+// my solution using set
 function recurring(arr) {
   let set_one = new Set()
   for (let i = 0; i < arr.length; i++) {
@@ -20,6 +21,18 @@ function recurring(arr) {
   return undefined
 }
 
-console.log(recurring([2, 5, 1, 2, 3, 5, 1, 2, 4]))
-console.log(recurring([2, 1, 1, 2, 3, 5, 1, 2, 4]))
-console.log(recurring([2, 3, 4, 5]))
+// instructor solution using objects
+function recurring2(arr) {
+  let map = {}
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] !== undefined) {
+      return arr[i]
+    } else {
+      map[arr[i]] = i
+    }
+  }
+}
+
+console.log(recurring2([2, 5, 1, 2, 3, 5, 1, 2, 4]))
+console.log(recurring2([2, 1, 1, 2, 3, 5, 1, 2, 4]))
+console.log(recurring2([2, 3, 4, 5]))
