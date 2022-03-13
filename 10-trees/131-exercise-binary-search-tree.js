c3f3372e509db085c74fd141bef7404d79c082ae
pang yew ken <ukenster@gmail.com>
2022-03-13 18:41:10 +0800

    Completed 132 exercise binary search tree

---
+++ 10-trees/131-exercise-binary-search-tree.js
@@ -0,0 +1,74 @@
+class Node {
+  constructor(value) {
+    this.left = null
+    this.right = null
+    this.value = value
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+  insert(value) {
+    const newNode = new Node(value)
+    if (!this.root) {
+      this.root = newNode
+    } else {
+      let currentNode = this.root
+      while (currentNode) {
+        if (newNode.value > currentNode.value) {
+          if (currentNode.right === null) {
+            currentNode.right = newNode
+            return
+          }
+          currentNode = currentNode.right
+        } else {
+          if (currentNode.left === null) {
+            currentNode.left = newNode
+            return
+          }
+          currentNode = currentNode.left
+        }
+      }
+    }
+  }
+
+  lookup(value) {
+    let currentNode = this.root
+    while (currentNode) {
+      if (value === currentNode.value) {
+        return currentNode
+      }
+      if (value > currentNode.value) {
+        currentNode = currentNode.right
+      } else {
+        currentNode = currentNode.left
+      }
+    }
+    return null
+  }
+  // remove
+}
+
+function traverse(node) {
+  const tree = { value: node.value }
+  tree.left = node.left === null ? null : traverse(node.left)
+  tree.right = node.right === null ? null : traverse(node.right)
+  return tree
+}
+
+const tree = new BinarySearchTree()
+tree.insert(9)
+console.log(tree)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+console.log(JSON.stringify(traverse(tree.root)))
+console.log(tree.lookup(1))
+//       9
+//    4     20
+//  1  6  15  170
