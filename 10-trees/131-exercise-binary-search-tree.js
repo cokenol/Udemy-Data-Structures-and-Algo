e1b4b776224c8c718d21ce08cf9f3cea0d544861
pang yew ken <ukenster@gmail.com>
2022-03-14 15:42:26 +0800

    Completed 135 BST remove()

--- 10-trees/131-exercise-binary-search-tree.js
+++ 10-trees/131-exercise-binary-search-tree.js
@@ -48,7 +48,86 @@ class BinarySearchTree {
     }
     return false
   }
-  // remove
+  remove(value) {
+    if (!this.root) {
+      return false
+    }
+    let found = this.root
+    let prev = null
+    // find the value in BST
+    while (found.value !== value) {
+      prev = found
+      if (value > found.value) {
+        found = found.right
+      } else {
+        found = found.left
+      }
+      if (!found) {
+        return false
+      }
+    }
+    console.log("remove=", value, "found=", found.value)
+    // Remove value from BST if found
+    // Option 1: No right child
+    if (!found.right) {
+      if (!prev) {
+        this.root = found.left
+      } else {
+        if (found.value < prev.value) {
+          prev.left = found.left
+        } else if (found.value > prev.value) {
+          prev.right = found.left
+        }
+      }
+      return
+    }
+    // Option 2 Right child which doesnt have a left child
+    if (!found.right.left) {
+      if (!prev) {
+        this.root = found.left
+      } else {
+        found.right.left = found.left
+        if (found.value < prev.value) {
+          prev.left = found.right
+        } else if (found.value > prev.value) {
+          prev.right = found.right
+        }
+      }
+      return true
+    }
+    // Option 3 Right child that has a left child
+    if (found.right) {
+      //find the Right child's left most child
+      let leftmost = found.right.left
+      let leftmostParent = found.right
+      while (leftmost.left) {
+        leftmostParent = leftmost
+        leftmost = leftmost.left
+      }
+      //Parent's left subtree is now leftmost's right subtree
+      leftmostParent.left = leftmost.right
+      leftmost.left = found.left
+      leftmost.right = found.right
+
+      if (!prev) {
+        this.root = leftmost
+      } else {
+        if (found.value < prev.value) {
+          prev.left = leftmost
+        } else if (found.value > prev.value) {
+          prev.right = leftmost
+        }
+      }
+      return true
+    }
+    // Option 4: No right and left child
+    if (!found.right && !found.left) {
+      if (!prev) {
+        this.root = null
+      }
+      return true
+    }
+  }
 }
 
 function traverse(node) {
@@ -60,17 +139,20 @@ function traverse(node) {
 
 const tree = new BinarySearchTree()
 tree.insert(9)
-console.log(tree)
+// console.log(tree)
 tree.insert(4)
 tree.insert(6)
 tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
+// console.log(JSON.stringify(traverse(tree.root)))
+// console.log(1, tree.lookup(1))
+// console.log(4, tree.lookup(4))
+// console.log(56, tree.lookup(56))
 console.log(JSON.stringify(traverse(tree.root)))
-console.log(1, tree.lookup(1))
-console.log(4, tree.lookup(4))
-console.log(56, tree.lookup(56))
+tree.remove(9)
+console.log(tree)
 //       9
 //    4     20
 //  1  6  15  170
