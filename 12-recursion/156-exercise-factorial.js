12bbbf1f1481bc428f6e22b646a3ca0c7f2bac3a
pang yew ken <ukenster@gmail.com>
2022-03-14 23:19:59 +0800

    Completed 156 exercise factorial

--- 12-recursion/156-exercise-factorial.js
+++ 12-recursion/156-exercise-factorial.js
@@ -1,1 +1,23 @@
-//  write two function that finds the factorial of any numbers
+//  write two function that finds the factorial of any numbers. One should use recursive, the other should just use a for loop
+
+// O(n)
+function findFactorialRecursive(number) {
+  // code here
+  if (number === 2) {
+    return 2
+  }
+  return number * findFactorialRecursive(number - 1)
+}
+
+// O(n)
+function findFactorialIterative(number) {
+  // code Here
+  let answer = 1
+  for (let i = number; i > 1; i--) {
+    answer *= i
+  }
+  return answer
+}
+
+console.log(findFactorialRecursive(6))
+console.log(findFactorialIterative(6))
