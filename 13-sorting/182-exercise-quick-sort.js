e42c477e1937a8cbe960501bf384d5af836d3397
pang yew ken <ukenster@gmail.com>
2022-03-16 14:00:41 +0800

    Completed 182 exercise quick sort

---
+++ 13-sorting/182-exercise-quick-sort.js
@@ -0,0 +1,37 @@
+function quickSort(arr) {
+  if (arr.length === 0) {
+    return arr
+  }
+  const pivot = arr[arr.length - 1]
+  let left = []
+  let right = []
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+  // console.log("left=", ...left, "right=", ...right)
+  // console.log(...left.concat(pivot).concat(right), "\n======================\n")
+  if (arr.length <= 3) {
+    return left.concat(pivot).concat(right)
+  }
+  return quickSort(left).concat(pivot).concat(quickSort(right))
+}
+
+const num = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0, 45]
+
+console.log(...num)
+console.log(...quickSort(num), "\n===========================\n")
+
+let nums = []
+for (let i = 0; i < 1000; i++) {
+  nums.push(Math.round(Math.random() * 100))
+}
+
+console.log(...nums, "\n===========================\n")
+console.log(nums.length)
+
+console.log(quickSort(nums).length)
+console.log(...quickSort(nums), "\n===========================\n")
