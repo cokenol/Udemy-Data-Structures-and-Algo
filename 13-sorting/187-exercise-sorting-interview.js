6688c0c7315380e5e4f49f690190bbe8598210e8
pang yew ken <ukenster@gmail.com>
2022-03-16 22:55:58 +0800

    Completed 187 sorting interviews

---
+++ 13-sorting/187-exercise-sorting-interview.js
@@ -0,0 +1,23 @@
+//#1 - Sort 10 schools around your house by distance:
+// Merge Sort
+
+//#2 - eBay sorts listings by the current Bid amount:
+// Merge sort
+
+//#3 - Sport scores on ESPN
+// Merge Sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// Heap Sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// Insertion Sort
+
+//#6 - Temperature Records for the past 50 years in Canada
+// Heap Sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+// Heap Sort
+
+//#8 - You want to teach sorting for the first time
+// Bubble Selection Sort.
