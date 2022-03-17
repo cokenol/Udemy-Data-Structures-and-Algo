6d0c57c516b1ff1e3d4a23cb12ef5a767f603fef
pang yew ken <ukenster@gmail.com>
2022-03-17 12:55:19 +0800

    Compelted 200 BFS DFS

---
+++ 13-sorting/199-exercise-bfs-vs-dfs.js
@@ -0,0 +1,24 @@
+// If you know a solution is not far from the root of the tree:
+// BFS
+BFS
+
+// If the tree is very deep and solutions are rare:
+// BFS
+BFS (DFS will take long)
+
+// If tree is very wide:
+// DFS
+DFS (BFS will need too much memory)
+
+// If the solutions are frequesnt but located deep in the tree
+// DFS
+DFS
+
+
+// Determinng whether a path exists between two nodes:
+// DFS
+DFS (built for this)
+
+// Finding the shortest path:
+// BFS
+BFS
