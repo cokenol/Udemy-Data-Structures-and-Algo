12cacbb29725d14e702a3a1f9c4097a124b1bb2b
pang yew ken <ukenster@gmail.com>
2022-03-17 12:41:07 +0800

    Added instructor answer for sorting

--- 13-sorting/187-exercise-sorting-interview.js
+++ 13-sorting/187-exercise-sorting-interview.js
@@ -1,23 +1,33 @@
 //#1 - Sort 10 schools around your house by distance:
 // Merge Sort
+Instruction Sort - Easy to code - Nearly sorted.
 
 //#2 - eBay sorts listings by the current Bid amount:
 // Merge sort
+Radix / Counting Sort - Bid is number
 
 //#3 - Sport scores on ESPN
-// Merge Sort
+// Merge Sort - Might be a little too much
+Quick Sort - Most efficient - Better space complexity.
 
 //#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
 // Heap Sort
+Merge Sort - Need performance - O(n log n)
 
 //#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
 // Insertion Sort
+Insertion sort - nearly sorted
 
 //#6 - Temperature Records for the past 50 years in Canada
 // Heap Sort
+Radix / Counting sort if temp = 36
+Quick sort for temp = 36.3
 
 //#7 - Large user name database needs to be sorted. Data is very random.
 // Heap Sort
+Quick Sort if database not too large
+
 
 //#8 - You want to teach sorting for the first time
 // Bubble Selection Sort.
+Bubble sort and selection sort
