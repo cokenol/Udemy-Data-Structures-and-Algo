5e21675fa30231228b1332ffa1344c3a1363eda6
pang yew ken <ukenster@gmail.com>
2022-03-18 19:25:48 +0800

    Keeping cache within function itself

--- 15-dynamic-programming/212-memoization.js
+++ 15-dynamic-programming/212-memoization.js
@@ -2,17 +2,21 @@ function addTo80(n) {
   return n + 80
 }
 
-let cache = {}
-function memoizedAddTo80(n) {
-  if (cache[n]) {
-    return cache[n]
-  } else {
-    console.log("long time")
-    cache[n] = n + 80
-    return cache[n]
+function memoizedAddTo80() {
+  let cache = {}
+  return function (n) {
+    if (cache[n]) {
+      return cache[n]
+    } else {
+      console.log("long time")
+      cache[n] = n + 80
+      return cache[n]
+    }
   }
 }
 
-console.log("1", memoizedAddTo80(5))
-console.log("2", memoizedAddTo80(6))
-console.log("3", memoizedAddTo80(5))
+const memized = memoizedAddTo80()
+
+console.log("1", memized(5))
+console.log("2", memized(6))
+console.log("3", memized(5))
