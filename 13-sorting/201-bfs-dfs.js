56ce99b93b9b7d16e3158f0ffd58237a7de175e9
pang yew ken <ukenster@gmail.com>
2022-03-18 13:50:59 +0800

    Completed exercise 205 validate a BST

--- 13-sorting/201-bfs-dfs.js
+++ 13-sorting/201-bfs-dfs.js
@@ -132,8 +132,9 @@ class BinarySearchTree {
     let list = []
     let queue = []
     queue.push(currentNode)
-
+    console.log(queue)
     while (queue.length > 0) {
+      console.log(queue)
       currentNode = queue.shift()
       console.log(currentNode.value)
       list.push(currentNode.value)
@@ -168,6 +169,26 @@ class BinarySearchTree {
   }
   DFSPostOrder() {
     return traversePostOrder(this.root, [])
+  }
+  isValidBST(root) {
+    if (!root) {
+      return false
+    }
+
+    const helper = (root, min, max) => {
+      if (!root) {
+        return true // We hit the end of the path
+      }
+
+      if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) {
+        return false // current node's val doesn't satisfy the BST rules
+      }
+
+      // Continue to scan left and right
+      return helper(root.left, min, root.val) && helper(root.right, root.val, max)
+    }
+
+    return helper(root, null, null)
   }
 }
 function traverseInOrder(node, list) {
@@ -211,20 +232,20 @@ function traversePostOrder(node, list) {
 // PostOrder - [1, 6, 4, 15, 170, 20, 9]
 
 const tree = new BinarySearchTree()
-tree.insert(9)
-tree.insert(4)
-tree.insert(6)
-tree.insert(20)
-tree.insert(170)
-tree.insert(15)
-tree.insert(1)
+// tree.insert(9)
+// tree.insert(4)
+// tree.insert(6)
+// tree.insert(20)
+// tree.insert(170)
+// tree.insert(15)
+// tree.insert(1)
 // tree.remove(170)
 // console.log(tree.breadthFirstSearch())
 // console.log(tree.breadthFirstSearchR([tree.root], []))
 // JSON.stringify(traverse(tree.root))
-console.log(...tree.DFSInOrder())
-console.log(...tree.DFSPreOrder())
-console.log(...tree.DFSPostOrder())
+// console.log(...tree.DFSInOrder())
+// console.log(...tree.DFSPreOrder())
+// console.log(...tree.DFSPostOrder())
 
 function traverse(node) {
   const tree = { value: node.value }
@@ -232,3 +253,47 @@ function traverse(node) {
   tree.right = node.right === null ? null : traverse(node.right)
   return tree
 }
+
+function isValidBST(root) {
+  if (!root) {
+    return false
+  }
+
+  const helper = (root, min, max) => {
+    if (!root) {
+      return true // We hit the end of the path
+    }
+    console.log("root=", root.value, "min", min, "max", max, (min !== null && root.value <= min) || (max !== null && root.value >= max) ? "false" : " true", "\n=============================\n")
+
+    if ((min !== null && root.value <= min) || (max !== null && root.value >= max)) {
+      return false // current node's value doesn't satisfy the BST rules
+    }
+
+    // Continue to scan left and right
+    return helper(root.left, min, root.value) && helper(root.right, root.value, max)
+  }
+
+  return helper(root, null, null)
+}
+/*        5
+      4       10
+    2  6    8  12
+   1          6  13*/
+five = new Node(5)
+four = new Node(4)
+ten = new Node(10)
+five.left = four
+five.right = ten
+ten.left = new Node(8)
+twelve = new Node(12)
+ten.right = twelve
+twelve.left = new Node(6)
+twelve.right = new Node(13)
+
+two = new Node(2)
+two.left = new Node(1)
+four.left = two
+six = new Node(6)
+four.right = six
+
+console.log(isValidBST(five))
